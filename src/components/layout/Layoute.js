53ebc7066acf6102df204a2a897f5f64ae6eb5e3
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-06-08 09:59:55 +0330

    Layout with styles

---
+++ src/components/layout/Layoute.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from "./Layoute.module.css"
+import Link from 'next/link'
+
+function Layoute({ children }) {
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.left}>
+                    <Link href="/">
+                        FoodApp
+                    </Link>
+                </div>
+                <div className={styles.right}>
+                    <Link href="/menu">Menu</Link>
+                    <Link href="/categories">Categories</Link>
+                </div>
+            </header>
+            <div className={styles.container}>{children}</div>
+            <footer className={styles.footer}>
+                <a href='https://github.com/Mani-Jebraeily' target='_blank' rel='noreferrer'>
+                Mani Jebraeily | Food App &copy;
+                </a>
+
+            </footer>
+        </>
+    )
+}
+
+export default Layoute
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import Layoute from "@/components/layout/Layoute";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layoute>
+      <Component {...pageProps} />
+    </Layoute>
+  );
 }
